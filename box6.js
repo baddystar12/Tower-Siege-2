b51c70307ba3ba4764532a9402b7a26773cfa65c
baddystar12 <72180686+baddystar12@users.noreply.github.com>
2020-11-24 21:51:51 -0800

    Update box6.js

--- box6.js
+++ box6.js
@@ -1,7 +1,7 @@
 class Box6 {
     constructor(x, y, width, height){
       var box6_options ={
-          isStatic:true
+          isStatic:false
       }
       this.body = Bodies.rectangle(x,y,width,height,box6_options);
       this.width = width;
@@ -14,7 +14,8 @@ class Box6 {
         fill("red");
         rect(position.x, position.y, this.width, this.height);
         if(this.body.speed<3){
-            this.display();
+            rectMode(CENTER);
+            rect();
         }
         else{
             World.remove(world, this.body);
