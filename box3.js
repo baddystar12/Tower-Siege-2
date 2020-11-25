2d8ac7678aeb9971671e49f1613fa1865c347610
baddystar12 <72180686+baddystar12@users.noreply.github.com>
2020-11-24 21:53:02 -0800

    Update box3.js

--- box3.js
+++ box3.js
@@ -1,7 +1,7 @@
 class Box3 {
     constructor(x, y, width, height){
       var box3_options ={
-          isStatic:true
+          isStatic:false
       }
       this.body = Bodies.rectangle(x,y,width,height,box3_options);
       this.width = width;
@@ -14,7 +14,8 @@ class Box3 {
         fill("#b7ff00");
         rect(position.x, position.y, this.width, this.height);
         if(this.body.speed<3){
-            this.display();
+            rectMode(CENTER);
+            rect();
         }
         else{
             World.remove(world, this.body);
