05724acfc84d9f6876d459454afd48858f11cbbd
baddystar12 <72180686+baddystar12@users.noreply.github.com>
2020-11-25 22:01:20 -0800

    Update box4.js

--- box4.js
+++ box4.js
@@ -10,18 +10,15 @@ class Box4 {
     }
     display(){
         var position = this.body.position;
-        rectMode(CENTER);
-        fill("#eba400");
-        rect(position.x, position.y, this.width, this.height);
         if(this.body.speed<3){
-            rectMode(CENTER);
-            rect();
+             rectMode(CENTER);
+             fill("#eba400");
+             rect(position.x, position.y, this.width, this.height);
         }
         else{
             World.remove(world, this.body);
             push();
             this.visibility = this.visibility-5;
-            tint(255, this.visibility);
             pop();
         }
     }
